988341b88c8e5fc57ed2279d5f2fd90a470007ab
DENIS TSITKO <denis.tsitko@gmail.com>
2022-09-16 16:05:38 +0300

    Some methods updated
    Constructor,  IsValidSymbol, ClearText, SplitWords, SplitnIntoWordsNoStop, SplitnIntoNgramsNoStop

--- TextComparator.cpp
+++ TextComparator.cpp
@@ -14,7 +14,19 @@ struct ComparisonStatisctics{
 class TextComparator{
 	
 public:
-	// TODO: constructor
+	explicit TextComparator(const string& stop_words_string)
+	: stop_words_(SplitStopWords(stop_words_string))
+	{
+	}
+	
+	template <typename StringContainer>
+	explicit TextComparator(const StringContainer& stop_words){
+		string stop_words_string = ""s;
+		for(const string& word: stop_words){
+			stop_words_string += word;
+		}
+		stop_words_ = SplitStopWords(stop_words_string);
+	}		
 	
 	void Feed(const string& text){
 		//TODO: method realization
@@ -28,23 +40,90 @@ private:
 	map<string, int> words_to_texts_;
 	map<string, int> ngrams_to_texts_;
 	set<string> stop_words_;
+	set<int> valid_symbols_;
 	int texts_count_;
 	
 	bool IsValidSymbol(const char c) const{
-		//TODO: method realization
+		if(valid_symbols_.empty()){ // initialization
+			valid_symbols_.insert(static_cast<char>(32)); // space
+			for(int i=48; i<=57; ++i){
+				valid_symbols_.insert(static_cast<char>(i)); // 0-9
+			}
+			for(int i=64; i<=90; ++i){
+				valid_symbols_.insert(static_cast<char>(i)); // @,A-Z
+			}
+			for(int i=97; i<=122; ++i){
+				valid_symbols_.insert(static_cast<char>(i)); // a-z
+			}
+		}
+		return valid_symbols_.count(c);
 	}
 	
 	
-	string CleanText(const string& text) const{
-		//TODO: method realization
+	string ClearText(const string& text) const{
+		string clean_text=""s;
+		for(const char c: text){
+			if(IsValidSymbol(c)){
+				clean_text+=c;
+			}
+		}
+		return clean_text;
+	}
+	
+	vector<string> SplitWords(const string& text) const{
+		string clean_text = ClearText(text);
+		string temp="";
+		vector<string> result;
+		for(const char c: clean_text){
+			if(c==' ' && !temp.empty()){
+				result.push_back(temp);
+				temp="";
+			} else{
+				temp+=c;
+			}
+		}
+		if(!temp.empty()){
+			result.push_back(temp);
+		}
+		return result;
+		
+	set<string> SplitStopWords(const string& text) const{
+		set<string> result;
+		for(const string& word: SplitWords(text)){
+			result.insert(word);
+		}
+		return result;
 	}
 	
 	vector<string> SplitnIntoWordsNoStop(const string& text) const{
-		//TODO: method realization
+		vector<string> result;
+		for(const string& word: SplitWords(text)){
+			if(!stop_words_.count(word)){
+				result.push_back(word);
+			}
+		}
+		return result;
 	}
 	
 	vector<string> SplitnIntoNgramsNoStop(const string& text) const{
-		//TODO: method realization
+		vector<string> result;
+		string ngram_string = ""s;
+		for(const string& word: SplitnIntoWordsNoStop(text)){
+			ngram_string += word;
+		}
+		string temp = ""s;
+		for(const char c: ngram_string){
+			if(temp.size() == NGRAM_SIZE){
+				result.push_back(temp);
+				temp = ""s;
+			} else{
+				temp += c;
+			}
+		}
+		if(!temp.empty()){
+			result.push_back(temp);
+		}
+		return result;
 	}
 	
 	double ComputeRelevance(const vector<string>& text1, const vector<string> text2) const{
