74183daa37d40b6f3bb05e33e633a0fe2c17bc66
DENIS TSITKO <denis.tsitko@gmail.com>
2022-09-19 15:29:30 +0300

    bugfix

--- TextComparator.cpp
+++ TextComparator.cpp
@@ -4,23 +4,54 @@
 #include <string>
 #include <vector>
 #include <numeric>
+#include <iostream>
+#include <algorithm>
 
 #ifndef NGRAM_SIZE
 #define NGRAM_SIZE 3
 #endif
 
+void print(const std::vector<std::string>& s){
+	for(const std::string w: s){
+		std::cout<<w<<" ";
+	}
+	std::cout<<std::endl;
+}
+
+void print(const std::map<std::string, std::set<int>>& s){
+	for(const auto [w, m]: s){
+		std::cout<<w<<": ";
+		for(int i: m){
+			std::cout<<i<<" ";
+		}
+	}
+	std::cout<<std::endl;
+}
+
 struct ComparisonStatisctics{
-	//TODO: constructor
+	ComparisonStatisctics(){}
+	
+	ComparisonStatisctics(double input_relevance,
+		double input_ngram_relevance,
+		double input_jordan_measure,
+		double input_ngram_jordan_measure){
+			relevance = input_relevance;
+			ngram_relevance = input_ngram_relevance;
+			jordan_measure = input_jordan_measure;
+			ngram_jordan_measure = input_ngram_jordan_measure;
+	}
 	
 	double relevance = 0.0;
 	double ngram_relevance = 0.0;
 	double jordan_measure = 0.0;
 	double ngram_jordan_measure = 0.0;
-}
+};
 
 class Line{
 public:
-	explicit Line(std::string& text): line_(text){}
+	Line(){}
+
+	explicit Line(const std::string& text): line_(text){}
 	
 	std::vector<std::string> SplitWordsToVec() {
 		std::string clean_text = ClearText(line_);
@@ -39,10 +70,11 @@ public:
 			}
 		}
 		return words_vec_;
+	}
 		
 	std::set<std::string> SplitWordsToSet() {
 		if(words_set_.empty()){
-			for(const std::string& word: SplitWordsToVec(line_)){
+			for(const std::string& word: SplitWordsToVec()){
 			words_set_.insert(word);
 			}
 		}
@@ -54,8 +86,9 @@ private:
 	std::string line_;
 	std::vector<std::string> words_vec_;
 	std::set<std::string> words_set_;
+	std::set<char> valid_symbols_;
 	
-	bool IsValidSymbol(const char c) const{
+	bool IsValidSymbol(const char c) {
 		if(valid_symbols_.empty()){ // initialization
 			valid_symbols_.insert(static_cast<char>(32)); // space
 			for(int i=48; i<=57; ++i){
@@ -72,11 +105,11 @@ private:
 	}
 	
 	
-	std::string ClearText(const std::string& text, std::string replacement = " "s) const{
-		std::string clean_text=""s;
+	std::string ClearText(const std::string& text, char replacement = ' ') {
+		std::string clean_text="";
 		for(const char c: text){
 			if(IsValidSymbol(c)){
-				clean_text += c;
+				clean_text += static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
 			} else{
 				clean_text += replacement;
 			}
@@ -87,11 +120,12 @@ private:
 
 class StopWords{
 public:
-	explicit StopWords(Line& line): stop_words_line_(line){
-		stop_words_set_ = stop_words_line_.SplitWordsToSet()
+	StopWords(const Line& line){
+		stop_words_line_=line;
+		stop_words_set_ = stop_words_line_.SplitWordsToSet();
 	}
 	
-	std::vector<std::string> SplitnIntoWordsNoStop(const Line& text) const{
+	std::vector<std::string> SplitnIntoWordsNoStop(Line& text) const{
 		std::vector<std::string> result;
 		for(const std::string& word: text.SplitWordsToVec()){
 			if(!stop_words_set_.count(word)){
@@ -102,21 +136,20 @@ public:
 	}
 	
 private:
-	Line stop_words_line_;
+	Line stop_words_line_=Line();
 	std::set<std::string> stop_words_set_;
 };
 
 class TextComparator{
-	
 public:
 	explicit TextComparator(const std::string& stop_words_string)
-	: stop_words_(StopWords(stop_words_string))
-	{
+	: stop_words_(StopWords(Line(stop_words_string))){
+
 	}
 	
 	template <typename StringContainer>
 	explicit TextComparator(const StringContainer& stop_words){
-		std::string stop_words_string = ""s;
+		std::string stop_words_string = "";
 		for(const std::string& word: stop_words){
 			stop_words_string += word;
 		}
@@ -125,58 +158,66 @@ public:
 	
 	void Feed(const std::string& text){
 		++texts_id_;
-		std::vector<std::string> words = SplitnIntoWordsNoStop(Line(text));
+		Line text_line= Line(text);
+		std::vector<std::string> words = SplitnIntoWordsNoStop(text_line);
+		print(SplitnIntoWordsNoStop(text_line));
 		for(const std::string& word: words){
-			words_to_texts_[word] = texts_id_;
+			words_to_texts_[word].insert(texts_id_);
 		}
-		std::vector<std::string> ngrams = SplitnIntoNgramsNoStop(Line(text));
+		std::vector<std::string> ngrams = SplitnIntoNgramsNoStop(text_line);
 		for(const std::string& ngram: ngrams){
-			ngrams_to_texts_[ngram] = texts_id_;
+			ngrams_to_texts_[ngram].insert(texts_id_);
 		}
 	}
 	
-	ComparisonStatisctics Compare(const std::string& text1, const std::string& text1){
+	ComparisonStatisctics Compare(const std::string& text1, const std::string& text2){
 		ComparisonStatisctics result;
-		result.relevance = ComputeRelevance(SplitnIntoWordsNoStop(Line(text1)), SplitnIntoWordsNoStop(Line(text2)));
-		result.ngram_relevance = ComputeNgramRelevance(SplitnIntoNgramsNoStop(Line(text1)), SplitnIntoNgramsNoStop(Line(text2)));
-		result.jordan_measure = ComputeJordanMeasure(SplitnIntoWordsNoStop(Line(text1)), SplitnIntoWordsNoStop(Line(text2)));
-		result.ngram_jordan_measure = ComputeNgramJordanMeasure(SplitnIntoNgramsNoStop(Line(text1)), SplitnIntoNgramsNoStop(Line(text2)));
+		Line text_line1= Line(text1);
+		Line text_line2= Line(text2);
+		result.relevance = ComputeRelevance(SplitnIntoWordsNoStop(text_line1), SplitnIntoWordsNoStop(text_line2));
+		result.ngram_relevance = ComputeNgramRelevance(SplitnIntoNgramsNoStop(text_line1), SplitnIntoNgramsNoStop(text_line2));
+		result.jordan_measure = ComputeJordanMeasure(SplitnIntoWordsNoStop(text_line1), SplitnIntoWordsNoStop(text_line2));
+		result.ngram_jordan_measure = ComputeNgramJordanMeasure(SplitnIntoNgramsNoStop(text_line1), SplitnIntoNgramsNoStop(text_line2));
 		return result;
 	}
 
 private:
-	std::map<std::string, int> words_to_texts_;
-	std::map<std::string, int> ngrams_to_texts_;
+	std::map<std::string, std::set<int>> words_to_texts_;
+	std::map<std::string, std::set<int>> ngrams_to_texts_;
 	StopWords stop_words_;
 	std::set<int> valid_symbols_;
 	int texts_id_=0;
 		
-	std::vector<std::string> SplitnIntoWordsNoStop(const Line& text) const{
+	std::vector<std::string> SplitnIntoWordsNoStop(Line& text) {
 		return stop_words_.SplitnIntoWordsNoStop(text);
 	}
 	
-	
-	
-	std::vector<std::string> SplitnIntoNgramsNoStop(const Line& text) const{
+	std::vector<std::string> SplitnIntoNgramsNoStop(Line& text) const{
 		std::vector<std::string> result;
-		std::string ngram_string = ""s;
+		std::string ngram_string = "";
 		for(const std::string& word: stop_words_.SplitnIntoWordsNoStop(text)){
 			ngram_string += word;
 		}
-		std::string temp = ""s;
-		for(const char c: ngram_string){
-			if(temp.size() == NGRAM_SIZE){
-				result.push_back(temp);
-				temp = ""s;
-			} else{
-				if(c!=' '){
-					temp += c;
+		std::string temp = "";
+		for(int i=0; i<ngram_string.size()- NGRAM_SIZE; ++i){
+			for(const char c: ngram_string.substr(i, ngram_string.size()-i)){
+				if(temp.size() == NGRAM_SIZE){
+					result.push_back(temp);
+					temp = "";
+					if(c!=' '){
+						temp += static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
+					}
+				} else{
+					if(c!=' '){
+						temp += static_cast<char>(std::tolower(static_cast<unsigned char>(c)));
+					}
 				}
 			}
+			if(!temp.empty()){
+				result.push_back(temp);
+			}
 		}
-		if(!temp.empty()){
-			result.push_back(temp);
-		}
+		
 		return result;
 	}
 	
@@ -197,6 +238,7 @@ private:
 		} else{
 			Idf = log(texts_id_);
 		}
+				std::cout<<Idf<<std::endl;
 		return Idf;
 	}
 	
@@ -204,20 +246,20 @@ private:
 			const std::vector<std::string>& words1, 
 			const std::vector<std::string>& words2) const{
 				
-		return (count(words1.begin(), words1.end(), word) + count(words2.begin(), words2.end(), word))/(words1.size() + words2.size());
+		return 1.0*(std::count(words1.begin(), words1.end(), word) + count(words2.begin(), words2.end(), word))/(words1.size() + words2.size());
 	}
 	
 	double ComputeNgramFreq(const std::string& ngram, 
 			const std::vector<std::string>& ngrams1, 
 			const std::vector<std::string>& ngrams2) const{
 				
-		return (count(ngrams1.begin(), ngrams1.end(), ngram) + count(ngrams2.begin(), ngrams2.end(), ngram))/(ngrams1.size() + ngrams2.size());
+		return 1.0*(std::count(ngrams1.begin(), ngrams1.end(), ngram) + std::count(ngrams2.begin(), ngrams2.end(), ngram))/(ngrams1.size() + ngrams2.size());
 	}
 	
 	double ComputeRelevance(const std::vector<std::string>& words1, const std::vector<std::string>& words2) const{
 		double relevance = 0.0;
 		for(const std::string& word: words1){
-			if(count(words2.begin(), words2.end(), word) > 0){
+			if(std::count(words2.begin(), words2.end(), word) > 0){
 				relevance += ComputeWordFreq(word, words1, words2)*ComputeWordIdf(word);
 			}
 		}			
@@ -227,7 +269,7 @@ private:
 	double ComputeNgramRelevance(const std::vector<std::string>& ngrams1, const std::vector<std::string>& ngrams2) const{
 		double relevance = 0.0;
 		for(const std::string& ngram: ngrams1){
-			if(count(ngrams2.begin(), ngrams2.end(), word) > 0){
+			if(count(ngrams2.begin(), ngrams2.end(), ngram) > 0){
 				relevance += ComputeNgramFreq(ngram, ngrams1, ngrams2)*ComputeNgramIdf(ngram);
 			}
 		}			
@@ -235,10 +277,31 @@ private:
 	}
 	
 	double ComputeJordanMeasure(const std::vector<std::string>& words1, const std::vector<std::string>& words2) const{
-		//TODO: method realization
+		double jordan_measure=0.0;
+		return jordan_measure;
 	}
 	
 	double ComputeNgramJordanMeasure(const std::vector<std::string>& ngrams1, const std::vector<std::string>& ngrams2) const{
-		//TODO: method realization
+		double ngram_jordan_measure = 0.0;
+		return ngram_jordan_measure;
 	}
 };
+
+
+
+void print(ComparisonStatisctics s){
+	std::cout<<s.relevance<<" "<<s.ngram_relevance<<std::endl;
+}
+
+int main(){
+	std::string s= "in the as a is am are with";
+	TextComparator t_c = TextComparator(s);
+	t_c.Feed("computer with 1tb ssd");
+	t_c.Feed("a bottle of whisky");
+	ComparisonStatisctics s1 = t_c.Compare("notebook with 512GB ssd", "computer with 1tb ssd");
+	ComparisonStatisctics s2 = t_c.Compare("notebook with 512GB ssd", "a bottle of whisky");
+	ComparisonStatisctics s3 = t_c.Compare("cmputer with 512GB ssd", "computer with 1tb ssd");
+	print(s1);
+	print(s2);
+	print(s3);
+}
